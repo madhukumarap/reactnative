06a3c079b0fa5142f20ed4ecb8c600923ee428ab
MADHUKUMARAP <124053300+madhukumarap@users.noreply.github.com>
2024-11-19 17:57:59 +0000

    upadtes

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -3,18 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{
+      backgroundColor:"red",
+      flex:1
+    }}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
